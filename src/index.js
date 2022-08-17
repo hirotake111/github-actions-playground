1aee4512034e51d75f39a217e1605735bf915c20
hirotake111 <hirotake111@users.noreply.github.com>
2022-08-17 13:04:09 +0900

    GitHub Actions test #13

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require("express")();
 
-app.get("/", (req, ress) => ress.json({ message: "hello world" }));
+app.get("/", (req, ress) => ress.json({ message: "hello WORLD" }));
 
 app.listen(3000, () => console.log("Listening on port 3000"));
